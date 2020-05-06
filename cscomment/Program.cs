540b90545c491b5d96cff705e4df2f28ede6e18b
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-05-06 20:26:29 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 2; y = 10;
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
